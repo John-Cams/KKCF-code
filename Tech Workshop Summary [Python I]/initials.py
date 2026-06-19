024310b54a9c3726d9604b3736fc9375112d63be
John-Cams <143854397+John-Cams@users.noreply.github.com>
2026-06-19 08:23:00 -0700

    Tech Workshop Summary [Python I]

---
+++ Tech Workshop Summary [Python I]/initials.py
@@ -0,0 +1,30 @@
+'''Instructions
+Create an initials.py program that displays your initials in block letters.
+
+First, start the program with a comment that says a fun fact about yourself.
+
+Then, create your block letters with your initials.
+
+For example, if your name is Dua Lipa, your initials would be D and L, and your block letters would look like this:
+
+DDDD   L
+D   D  L
+D   D  L
+D   D  L
+D   D  L
+D   D  L
+DDDD   LLLLL
+'''
+
+# Write code below 💖
+
+#I have coded in Python since I was a kid
+
+print('JJJJJJJ L        M       M')
+print('    J   L        MM     MM')
+print('    J   L        M M   M M')
+print('    J   L        M  M M  M')
+print('    J   L        M   M   M')
+print('    J   L        M   M   M')
+print(' J  J   L        M       M')
+print('  JJ    LLLLLLLL M       M')
